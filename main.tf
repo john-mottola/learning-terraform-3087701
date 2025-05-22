cafa9b5923477bbf8861144f3898a1f6d4f78a03
john-mottola <john.l.mottola@gmail.com>
2025-05-22 10:20:20 -0400

    remove target goup

--- main.tf
+++ main.tf
@@ -42,7 +42,6 @@ module "autoscaling" {
   max_size = 2
 
   vpc_zone_identifier = module.blog_vpc.public_subnets
-  target_group_arns   = module.blog_alb.target_group_arns
   security_groups     = [module.blog_sg.security_group_id]
 
   image_id        = data.aws_ami.app_ami.id
@@ -67,16 +66,6 @@ module "blog_alb" {
       forward = {
         target_group_key = "ex-instance"
       }
-    }
-  }
-
-  target_groups = {
-    ex-instance = {
-      name_prefix      = "blog-"
-      protocol         = "HTTP"
-      port             = 80
-      target_type      = "instance"
-      target_id        = aws_instance.blog.id
     }
   }
 
