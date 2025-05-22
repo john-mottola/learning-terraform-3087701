c6e8794cf43c039376014cb8d531396e54738a54
john-mottola <john.l.mottola@gmail.com>
2025-05-21 21:25:32 -0400

    3.7

--- main.tf
+++ main.tf
@@ -28,9 +28,24 @@ resource "aws_instance" "blog" {
   }
 }
 
+module "blog_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "5.3.0"
+  name    = "blog_new"
+
+  vpc_id              = data.aws_vpc.default.id
+  ingress_rules       = ["http-80-tcp","https-443-tcp"]
+  ingress_cidr_blocks = [0.0.0.0/0]
+
+  egress_rules        = ["all-all"]
+  egress_cidr_blocks  = [0.0.0.0/0]
+}
+
 resource "aws_security_group" "blog" {
   name = "blog"
   description = "Allow http(s) in. Allow everything out."
+
+  vpc_security_group_ids = [module.blog_sg.security_group_id]
 
   vpc_id      = data.aws_vpc.default.id
 }
