b6769a05de68ae967dd082b92ec6ad53c911f761
john-mottola <john.l.mottola@gmail.com>
2025-05-21 20:06:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -17,7 +17,6 @@ data "aws_ami" "app_ami" {
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = "t3.nano"
-  vpc_id = "vpc-05b27aa5c2e543e3f"
 
   tags = {
     Name = "HelloWorld"
