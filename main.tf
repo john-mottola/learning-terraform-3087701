6518347014ec5efcd930491c7db9ec42f74db1b0
john-mottola <john.l.mottola@gmail.com>
2025-05-22 09:43:44 -0400

    9

--- main.tf
+++ main.tf
@@ -45,7 +45,7 @@ resource "aws_instance" "blog" {
 
 module "blog_alb" {
   source  = "terraform-aws-modules/alb/aws"
-  version = "~> 6.0"
+  version = "~> 9.0"
 
   name                = "blog-alb"
   vpc_id              = module.blog_vpc.vpc_id
